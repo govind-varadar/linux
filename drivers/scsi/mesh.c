cd81ad38d7a343f5f01eb1a7a546fc8800727ac9
Hannes Reinecke <hare@suse.de>
2021-01-25 16:59:13 +0100

    mesg: translate message to host byte status
    Instead of setting the message byte translate it to a host byte
    status. As the error recovery would map it to DID_ERROR anyway
    the translation doesn't change the SCSI error handling.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- drivers/scsi/mesh.c
+++ drivers/scsi/mesh.c
@@ -597,8 +597,9 @@ static void mesh_done(struct mesh_state *ms, int start_next)
 	if (cmd) {
 		set_host_byte(cmd, ms->stat);
 		set_status_byte(cmd, cmd->SCp.Status);
-		if (ms->stat == DID_OK)
-			set_msg_byte(cmd, cmd->SCp.Message);
+		if (get_host_byte(cmd) == DID_OK &&
+		    cmd->SCp.Message != COMMAND_COMPLETE)
+			translate_msg_byte(cmd, cmd->SCp.Message);
 		if (DEBUG_TARGET(cmd)) {
 			printk(KERN_DEBUG "mesh_done: result = %x, data_ptr=%d, buflen=%d\n",
 			       scsi_get_compat_result(cmd), ms->data_ptr, scsi_bufflen(cmd));
