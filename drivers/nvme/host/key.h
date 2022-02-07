a7cd6dbbec89fb3cbde1b298530073d376a77290
Hannes Reinecke <hare@suse.de>
2022-02-07 12:39:22 +0100

    nvme-key: remove non-existing psk definitions

--- drivers/nvme/host/key.h
+++ drivers/nvme/host/key.h
@@ -6,18 +6,10 @@
 #ifndef _NVME_KEY_H
 #define _NVME_KEY_H
 
-struct key *nvme_keyring_insert_psk(struct key *nvme_key,
-				    char *hostnqn, int hmac);
-struct key *nvme_keyring_lookup_psk(char *hostnqn, int hash);
 struct key *nvme_keyring_insert_dhchap(char *hostnqn, char *subnqn, int hmac,
 				       void *key_data, size_t key_len);
 struct key *nvme_keyring_lookup_dhchap(char *hostnqn, char *subnqn, int hash);
 void nvme_keyring_revoke_dhchap(char *hostnqn, char *subnqn, int hash);
-struct key *nvme_keyring_insert_tls(struct key *nvme_key,
-				    char *hostnqn, char *subnqn, int hmac,
-				    bool generated);
-struct key *nvme_keyring_lookup_tls(char *hostnqn, char *subnqn,
-				    int hash, bool generated);
 
 int nvme_keyring_init(void);
 void nvme_keyring_exit(void);
