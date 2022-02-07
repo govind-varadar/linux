fd704a1735c25108f193e4dcebb5b3508ec33e3d
Hannes Reinecke <hare@suse.de>
2022-02-07 13:17:04 +0100

    nvme-key: add helper functions to derive NVMe TLS keys
    Add helper functions 'nvme_keyring_insert_tls()' and
    'nvme_keyring_lookup_tls()' to derive and lookup NVMe TLS keys.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- drivers/nvme/host/key.c
+++ drivers/nvme/host/key.c
@@ -6,9 +6,12 @@
 #include <linux/nvme.h>
 #include <crypto/hash.h>
 #include <crypto/hkdf.h>
+#include <net/tls.h>
 #include <keys/user-type.h>
 #include <asm/unaligned.h>
 
+#include "nvme.h"
+#include "fabrics.h"
 #include "key.h"
 
 static struct key *nvme_keyring;
@@ -275,6 +278,168 @@ struct key *nvme_keyring_lookup_retained_key(char *hostnqn, int hmac)
 	return key_ref_to_ptr(keyref);
 }
 EXPORT_SYMBOL_GPL(nvme_keyring_lookup_retained_key);
+
+struct key *nvme_keyring_insert_tls(struct key *nvme_key,
+		struct nvme_ctrl *ctrl, int hmac, bool generated)
+{
+	char *hostnqn = ctrl->opts->host->nqn;
+	char *subnqn = nvmf_ctrl_subsysnqn(ctrl);
+	struct crypto_shash *hmac_tfm;
+	const char *hmac_name = "hmac(sha256)";
+	const char *psk_prefix = "tls13 nvme-tls-psk";
+	char *identity, *tls_identity;
+	size_t identity_len = (NVMF_NQN_SIZE) * 2 + 11, tls_identity_len;
+	key_ref_t keyref;
+	struct user_key_payload *key_payload;
+	size_t infolen, key_len = 32;
+	char *info;
+	unsigned char *prk, *tls_key;
+	int ret;
+
+	if (!ctrl->opts)
+		return ERR_PTR(-ENXIO);
+
+	identity = kzalloc(identity_len, GFP_KERNEL);
+	if (!identity)
+		return ERR_PTR(-ENOMEM);
+
+	snprintf(identity, identity_len, "NVMe0%c%02d %s %s",
+		 generated ? 'G' : 'R', hmac, hostnqn, subnqn);
+
+	tls_identity_len = strlen(identity) + strlen(ctrl->opts->traddr) + 4;
+	if (ctrl->opts->host_traddr)
+		tls_identity_len += strlen(ctrl->opts->host_traddr);
+	if (ctrl->opts->trsvcid)
+		tls_identity_len += strlen(ctrl->opts->trsvcid);
+	tls_identity = kzalloc(tls_identity_len, GFP_KERNEL);
+	if (!tls_identity) {
+		ret = -ENOMEM;
+		goto out_free_identity;
+	}
+	snprintf(tls_identity, tls_identity_len, "%s;%s;%s;%s",
+		 ctrl->opts->host_traddr ? ctrl->opts->host_traddr : "",
+		 ctrl->opts->traddr,
+		 ctrl->opts->trsvcid ? ctrl->opts->trsvcid : "",
+		 identity);
+
+	key_payload = nvme_key->payload.data[0];
+	key_len = key_payload->datalen;
+
+	if (hmac == 2) {
+		hmac_name = "hmac(sha384)";
+		key_len = 48;
+	}
+
+	hmac_tfm = crypto_alloc_shash(hmac_name, 0, 0);
+	if (IS_ERR(hmac_tfm)) {
+		ret = PTR_ERR(hmac_tfm);
+		goto out_free_tls_identity;
+	}
+
+	prk = kzalloc(crypto_shash_digestsize(hmac_tfm), GFP_KERNEL);
+	if (!prk) {
+		ret = -ENOMEM;
+		goto out_free_shash;
+	}
+
+	ret = hkdf_extract(hmac_tfm, key_payload->data,
+			   key_payload->datalen, prk);
+	if (ret)
+		goto out_free_prk;
+
+	ret = crypto_shash_setkey(hmac_tfm, prk, key_payload->datalen);
+	if (ret)
+		goto out_free_prk;
+
+	infolen = strlen(identity) + strlen(psk_prefix) + 1;
+	info = kzalloc(infolen, GFP_KERNEL);
+	if (!info)
+		goto out_free_prk;
+
+	memcpy(info, psk_prefix, strlen(psk_prefix));
+	memcpy(info + strlen(psk_prefix), identity, strlen(identity));
+
+	tls_key = kzalloc(key_len, GFP_KERNEL);
+	if (!tls_key) {
+		ret = -ENOMEM;
+		goto out_free_info;
+	}
+	ret = hkdf_expand(hmac_tfm, info, strlen(info), tls_key, key_len);
+	if (ret)
+		goto out_free_key;
+	pr_debug("refresh tls psk '%s'\n", tls_identity);
+	keyref = tls_psk_refresh(tls_identity, tls_key, key_len);
+	if (IS_ERR(keyref)) {
+		pr_warn("refresh tls psk '%s' failed, error %ld\n",
+			tls_identity, PTR_ERR(keyref));
+		ret = -ENOKEY;
+		goto out_free_key;
+	}
+	ret = 0;
+
+out_free_key:
+	kfree(tls_key);
+out_free_info:
+	kfree(info);
+out_free_prk:
+	kfree(prk);
+out_free_shash:
+	crypto_free_shash(hmac_tfm);
+out_free_tls_identity:
+	kfree(tls_identity);
+out_free_identity:
+	kfree(identity);
+
+	return ret ? ERR_PTR(ret) : key_ref_to_ptr(keyref);
+}
+EXPORT_SYMBOL_GPL(nvme_keyring_insert_tls);
+
+struct key *nvme_keyring_lookup_tls(struct nvme_ctrl *ctrl,
+		int hmac, bool generated)
+{
+	char *hostnqn = ctrl->opts->host->nqn;
+	char *subnqn = nvmf_ctrl_subsysnqn(ctrl);
+	char *identity;
+	char *host_traddr = ctrl->opts->host_traddr;
+	char *traddr = ctrl->opts->traddr;
+	char *trsvcid = ctrl->opts->trsvcid;
+	size_t identity_len = (NVMF_NQN_SIZE) * 2 + 11;
+	key_ref_t keyref;
+
+	identity = kzalloc(identity_len, GFP_KERNEL);
+	if (!identity)
+		return ERR_PTR(-ENOMEM);
+retry:
+	snprintf(identity, identity_len, "%s;%s;%s;NVMe0%c%02d %s %s",
+		 host_traddr ? host_traddr : "",
+		 traddr ? traddr : "", trsvcid ? trsvcid : "",
+		 generated ? 'G' : 'R', hmac, hostnqn, subnqn);
+
+	pr_debug("lookup tls psk '%s'\n", identity);
+	keyref = tls_psk_lookup(identity);
+	if (IS_ERR(keyref)) {
+		if (host_traddr) {
+			host_traddr = NULL;
+			goto retry;
+		}
+		if (trsvcid) {
+			trsvcid = NULL;
+			goto retry;
+		}
+		if (traddr) {
+			traddr = NULL;
+			goto retry;
+		}
+		pr_debug("lookup tls psk '%s' failed, error %ld\n",
+			 identity, PTR_ERR(keyref));
+		kfree(identity);
+		return ERR_PTR(-ENOKEY);
+	}
+	kfree(identity);
+
+	return key_ref_to_ptr(keyref);
+}
+EXPORT_SYMBOL_GPL(nvme_keyring_lookup_tls);
 
 int nvme_keyring_init(void)
 {
--- drivers/nvme/host/key.h
+++ drivers/nvme/host/key.h
@@ -14,6 +14,10 @@ void nvme_keyring_revoke_generated_key(char *hostnqn, char *subnqn, int hmac);
 struct key *nvme_keyring_insert_retained_key(struct key *nvme_key,
 		char *hostnqn, int hmac);
 struct key *nvme_keyring_lookup_retained_key(char *hostnqn, int hmac);
+struct key *nvme_keyring_insert_tls(struct key *nvme_key,
+		struct nvme_ctrl *ctrl, int hmac, bool generated);
+struct key *nvme_keyring_lookup_tls(struct nvme_ctrl *ctrl,
+		int hmac, bool generated);
 
 int nvme_keyring_init(void);
 void nvme_keyring_exit(void);
