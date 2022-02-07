25cdf7ec4d5d79a5561c2903fa56ca74f984db8e
Hannes Reinecke <hare@suse.de>
2022-02-07 12:22:22 +0100

    nvme: add nvme_auth_generate_psk()
    Add a function to generate a NVMe PSK from the shared credentials
    negotiated by DH-HMAC-CHAP.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- drivers/nvme/host/key.c
+++ drivers/nvme/host/key.c
@@ -14,6 +14,136 @@
 
 static struct key *nvme_keyring;
 
+static int nvme_key_preparse(struct key_preparsed_payload *prep)
+{
+	struct user_key_payload *upayload;
+	int datalen = prep->datalen;
+	const char *data = prep->data;
+
+	if (datalen <= 0 || !data)
+		return -EINVAL;
+
+	prep->quotalen = datalen;
+	upayload = kmalloc(sizeof(*upayload) + datalen + 1, GFP_KERNEL);
+	if (!upayload)
+		return -ENOMEM;
+	upayload->datalen = datalen;
+	memcpy(upayload->data, data, datalen);
+	upayload->data[datalen] = '\0';
+	prep->payload.data[0] = upayload;
+	return 0;
+}
+
+static void nvme_key_free_preparse(struct key_preparsed_payload *prep)
+{
+	kfree(prep->payload.data[0]);
+}
+
+static void nvme_key_describe(const struct key *key, struct seq_file *m)
+{
+	seq_puts(m, key->description);
+	seq_printf(m, ": %u", key->datalen);
+}
+
+static struct key_type key_type_nvme_dhchap = {
+	.name           = "dhchap",
+	.flags          = KEY_TYPE_NET_DOMAIN,
+	.preparse       = nvme_key_preparse,
+	.free_preparse  = nvme_key_free_preparse,
+	.instantiate    = generic_key_instantiate,
+	.revoke         = user_revoke,
+	.destroy        = user_destroy,
+	.describe       = nvme_key_describe,
+	.read           = user_read,
+};
+
+struct key *nvme_keyring_insert_dhchap(char *hostnqn, char *subnqn, int hash,
+				       void *key_data, size_t key_len)
+{
+	char *identity;
+	size_t identity_len = (NVMF_NQN_SIZE * 2) + 5;
+	key_ref_t keyref;
+	key_perm_t keyperm =
+		KEY_POS_SEARCH | KEY_POS_VIEW | KEY_POS_READ |
+		KEY_USR_SEARCH | KEY_USR_VIEW | KEY_USR_READ;
+
+	identity = kzalloc(identity_len, GFP_KERNEL);
+	if (!identity)
+		return ERR_PTR(-ENOMEM);
+
+	snprintf(identity, identity_len, "%02d %s %s",
+		hash, hostnqn, subnqn);
+
+	/* create or update key */
+	pr_debug("update dhchap key '%s'\n", identity);
+	keyref = key_create_or_update(make_key_ref(nvme_keyring, true),
+				      "dhchap", identity, key_data, key_len,
+				      keyperm, KEY_ALLOC_NOT_IN_QUOTA);
+	if (IS_ERR(keyref)) {
+		pr_warn("failed to update dhchap key '%s', error %ld\n",
+			identity, PTR_ERR(keyref));
+		kfree(identity);
+		return ERR_PTR(-ENOKEY);
+	}
+	return key_ref_to_ptr(keyref);
+}
+EXPORT_SYMBOL_GPL(nvme_keyring_insert_dhchap);
+
+void nvme_keyring_revoke_dhchap(char *hostnqn, char *subnqn, int hash)
+{
+	char *identity;
+	size_t identity_len = (NVMF_NQN_SIZE * 2) + 5;
+	struct key *key;
+	key_ref_t keyref;
+
+	identity = kzalloc(identity_len, GFP_KERNEL);
+	if (!identity)
+		return;
+
+	snprintf(identity, identity_len, "%02d %s %s",
+		hash, hostnqn, subnqn);
+
+	/* register key */
+	keyref = keyring_search(make_key_ref(nvme_keyring, true),
+				&key_type_nvme_dhchap,
+				identity, false);
+	if (IS_ERR(keyref))
+		return;
+
+	key = key_ref_to_ptr(keyref);
+	key_invalidate(key);
+	key_put(key);
+	kfree(identity);
+}
+EXPORT_SYMBOL_GPL(nvme_keyring_revoke_dhchap);
+
+struct key *nvme_keyring_lookup_dhchap(char *hostnqn, char *subnqn, int hash)
+{
+	char *identity;
+	size_t identity_len = (NVMF_NQN_SIZE * 2) + 5;
+	key_ref_t keyref;
+
+	identity = kzalloc(identity_len, GFP_KERNEL);
+	if (!identity)
+		return ERR_PTR(-ENOMEM);
+
+	snprintf(identity, identity_len, "%02d %s %s", hash, hostnqn, subnqn);
+
+	pr_debug("lookup dhchap key '%s'\n", identity);
+	keyref = keyring_search(make_key_ref(nvme_keyring, true),
+				&key_type_nvme_dhchap, identity, false);
+
+	kfree(identity);
+	if (IS_ERR(keyref)) {
+		pr_debug("lookup dhchap key '%s' failed, error %ld\n",
+			 identity, PTR_ERR(keyref));
+		return ERR_PTR(-ENOKEY);
+	}
+
+	return key_ref_to_ptr(keyref);
+}
+EXPORT_SYMBOL_GPL(nvme_keyring_lookup_dhchap);
+
 int nvme_keyring_init(void)
 {
 	int result;
@@ -46,7 +176,6 @@ out_revoke:
 
 void nvme_keyring_exit(void)
 {
-	unregister_key_type(&key_type_nvme_psk);
 	unregister_key_type(&key_type_nvme_dhchap);
 	key_revoke(nvme_keyring);
 	key_put(nvme_keyring);
